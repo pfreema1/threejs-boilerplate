516814c04a31c81662d9b090213538ee371a2ea0
pfreema1 <paul@level2d.com>
2020-01-12 19:09:33 -0500

    foo

--- src/scripts/RenderTri.js
+++ src/scripts/RenderTri.js
@@ -6,63 +6,66 @@ import fullScreenTriFrag from './../shaders/fullScreenTri.frag';
 import fullScreenTriVert from './../shaders/fullScreenTri.vert';
 
 export default class RenderTri {
-    constructor(scene, renderer, bgRenderTarget, mouseCanvas, textCanvas) {
-        this.scene = scene;
-        this.renderer = renderer;
-        this.bgRenderTarget = bgRenderTarget;
-        this.mouseCanvas = mouseCanvas;
-        this.textCanvas = textCanvas;
+  constructor(scene, renderer, bgRenderTarget, mouseCanvas, textCanvas) {
+    this.scene = scene;
+    this.renderer = renderer;
+    this.bgRenderTarget = bgRenderTarget;
+    this.mouseCanvas = mouseCanvas;
+    this.textCanvas = textCanvas;
 
-        const resolution = new THREE.Vector2();
-        this.renderer.getDrawingBufferSize(resolution);
+    const resolution = new THREE.Vector2();
+    this.renderer.getDrawingBufferSize(resolution);
 
-        this.RenderTriTarget = new THREE.WebGLRenderTarget(
-            resolution.x,
-            resolution.y,
-            {
-                format: THREE.RGBFormat,
-                stencilBuffer: false,
-                depthBuffer: true
-            }
-        );
+    this.RenderTriTarget = new THREE.WebGLRenderTarget(
+      resolution.x,
+      resolution.y,
+      {
+        format: THREE.RGBFormat,
+        stencilBuffer: false,
+        depthBuffer: true
+      }
+    );
 
-        this.triMaterial = new THREE.RawShaderMaterial({
-            fragmentShader: glslify(fullScreenTriFrag),
-            vertexShader: glslify(fullScreenTriVert),
-            uniforms: {
-                uScene: {
-                    type: 't',
-                    value: this.bgRenderTarget.texture
-                },
-                uMouseCanvas: {
-                    type: 't',
-                    value: this.mouseCanvas.texture
-                },
-                uTextCanvas: {
-                    type: 't',
-                    value: this.textCanvas.texture
-                },
-                uResolution: { value: resolution },
-                uTime: {
-                    value: 0.0
-                }
-            }
-        });
+    this.triMaterial = new THREE.RawShaderMaterial({
+      fragmentShader: glslify(fullScreenTriFrag),
+      vertexShader: glslify(fullScreenTriVert),
+      uniforms: {
+        uScene: {
+          type: 't',
+          value: this.bgRenderTarget.texture
+        },
+        uMouseCanvas: {
+          type: 't',
+          value: this.mouseCanvas.texture
+        },
+        uTextCanvas: {
+          type: 't',
+          value: this.textCanvas.texture
+        },
+        uResolution: { value: resolution },
+        uTime: {
+          value: 0.0
+        }
+      }
+    });
 
-        let renderTri = new THREE.Mesh(this.returnRenderTriGeometry(), this.triMaterial);
-        renderTri.frustumCulled = false;
+    let renderTri = new THREE.Mesh(
+      this.returnRenderTriGeometry(),
+      this.triMaterial
+    );
+    renderTri.frustumCulled = false;
 
-        this.scene.add(renderTri);
-    }
+    this.scene.add(renderTri);
+  }
 
-    returnRenderTriGeometry() {
-        const geometry = new THREE.BufferGeometry();
+  returnRenderTriGeometry() {
+    const geometry = new THREE.BufferGeometry();
 
-        // triangle in clip space coords
-        const vertices = new Float32Array([-1.0, -1.0, 3.0, -1.0, -1.0, 3.0]);
+    // triangle in clip space coords
+    const vertices = new Float32Array([-1.0, -1.0, 3.0, -1.0, -1.0, 3.0]);
 
-        geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 2));
+    geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 2));
 
-        return geometry;
-    }
+    return geometry;
+  }
 }
